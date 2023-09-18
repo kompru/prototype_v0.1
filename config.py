815309c9a81ff4df3534ef8afeb0f57c68e277db
Dahsleman <dahsleman2023@gmail.com>
2023-09-18 18:37:32 -0300

    add config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+
+config = {
+
+    "type": "service_account",
+    "project_id": "carbide-booth-398715",
+    "private_key_id": "8208869cc0c98fdb49bfdb1e7735c1709a885c78",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDAbSFz5R7z/3v4\nk4f3XlYDj5jx2iwX/QVzIbJgVGyz7866JpZwzNXPK9pFIsu3EoOnaN0UVdgHbeUe\nsI2iIAeTx+TyaPERY3AbQPj70TKcJwB0mC92KLUMQ+4b+a0e++UanrBipm48nl+S\nSWs8ybw5hU/+eaRVotO9RdeBs70i40dm9cBeu0WfkKjmnuWXfH31j3iRzaf7N5KG\n45Ld8oPuJPTJTjlOmoEZ2wP5Mh21T84R5BZthCynwFxHLWebmEbcbNZQIuY13Orx\nuFzJagw2ntTXg2yQOmwQTx/uAb6ocLSl33easGxKJKwprdER3KoikpGDmXfq18uZ\nWDvel7dzAgMBAAECggEAHMXUrDc03ygzGdK5kHgHJYYpyPQmBro7P+FogjXtgSRZ\nBYWylhBS4iT9ALx4uJmonPY1pI5HfKjmiyW8L+NZLrRDnCmr7j0vrKuU71ZzGl1M\nmKKO6jyhAcIAjydQxyLEOaxD1KRtNFw8DywyQEcsAW/ZK8R1maZ8wb75P/gnzljq\nCVaHcN+oL7htgP08QPLJ7Vp+04AzKRBDZtQ/huTWqVajFKLclNa8fizSTXiBRA4l\nkfTjMsPYbXE6BzgPZW7pG7scCnFgPxB4DER/7YUT+oq+U9O7dlrvbUQGcr5ZeNVt\nsofeNdqqCsJ0+X2eyesSd5BOIZqvIzTp0PVGxJLYFQKBgQD0w5BXuDPw+wpmkFUy\nDVALwZUGgGyo0dKpyOxkv4x0Ssiv4nZQJlxmljFKFDXHm+9UmbhFQCz20W8jdy2W\n45JEHIZ3g/ss50duy7cv31lnpEXM/PCjdRw9tCJqagoLiGen4EiBLCB4GaNKK5uD\nm4XGGCyr8jo9zW+oobTbqY1ohQKBgQDJQoBVyqsXr5/DXVXG+JWtCwtbJPU+tscw\nDryCiLaCAOAyRLHRDYJ5UeA756fEeHTmZyO6TT/t0cfgfUoA+5JSC25dFcLXiKNu\nn4tTckY1r6chkiFtSqcdVc077atwUmQ78Hu1j4v/svEEQv8iLJUiOYgCYn9nutcm\nJQ1NRXgdlwKBgQCy3UbaE45Bh/KtyMO5c+kO7kQNIERHii28TUwSw7fMM+o8Pe3f\n3Gihvxf93o7+c4GN/Vefo6o4JAxnimwSW/NDOuU46YXf5pNY8B3KKhmXl4A6sIzZ\npncg0EZmJV951hCJqWjSnICNw46EYSGgJ0K25Gugq77L8xipzqHv9BOWUQKBgQCi\nnLDEoLQMt0IGv+sGkOOepUXPptTgjx3xnnaBoaSU9dAvzYE+wAOjspvSyyGqhQkv\nADbF2JTGurEcmR6qHJnk4IeTfnmnXCdwwkf4bGR+DCal4URQ4fiUozboLOhQIMIM\nDApocEtVlmdWYitymUMGHCXgh2UZksUzdMyCmpaqkQKBgQDRr8itWPYsUsVQS+UB\nBU1X6T0pGw2I11JREIAbNJetUSkYMIH+fsub6m5vRhU8QF9Lr4KOATlHjfldLJTv\nFRzBJFP8tOtOoA0IXVLvVjzXDnoqlkYxtOF4nD6fa/3mlpltE7ObtyI3RFF37kWs\n7jOzsd0g7b8mT8k/5x95SOdXXA==\n-----END PRIVATE KEY-----\n",
+    "client_email": "kompru-teste@carbide-booth-398715.iam.gserviceaccount.com",
+    "client_id": "114434679126567387875",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/kompru-teste%40carbide-booth-398715.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com",
+}
+
+products_spreadsheetid = "11MMXmd2PNtDk8BfPe5BALj-IRY8gcoPWome4F-CF_7s"
